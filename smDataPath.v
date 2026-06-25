b86e70d66a470e5fc09cee49358bd6ff3f8b85af
huynhmel <138836790+Nivlmin@users.noreply.github.com>
2026-06-25 02:35:44 -0600

    Update smDataPath.v

--- smDataPath.v
+++ smDataPath.v
@@ -1,11 +1,13 @@
-module smDataPath (clk, reset, coin_in, clearGame, spinA, spinB, spinC, gameStart, jackpot, pairWin, hexA, hexB, hexC);
+module smDataPath (clk, reset, coin_in, clearGame, spinA, spinB, spinC, gameStart, jackpot, pairWin, setSeed, hexA, hexB, hexC);
 
 	input wire clk, reset, coin_in, clearGame, spinA, spinB, spinC;
+	input wire [7:0] setSeed; // 255 Different possiblities for the slot machine 
 	output wire gameStart, jackpot, pairWin;
 	output wire [6:0] hexA, hexB, hexC;
 	
 	// Internal Wires
 	wire [7:0] lfsr_bus, reelA_out, reelB_out, reelC_out;
+	reg [7:0] mux_lfsr;
 	reg gameStart_reg;
 	// Game Logic
 	always @(posedge clk or negedge reset)
@@ -29,10 +31,22 @@ module smDataPath (clk, reset, coin_in, clearGame, spinA, spinB, spinC, gameStar
 	// Seed/ Random Number Generation (LFSR)
 	smLFSR rngMachine(reelA_out, lfsr_bus);
 	
+	always @(*)
+	begin
+		if (reelA_out == 8'h00)
+		begin
+			mux_lfsr = setSeed; // Custom Seed
+		end
+		else
+		begin
+			mux_lfsr = lfsr_bus; // Normal scrambling for a randomized Seed
+		end
+	end
+	
 	// Three Reel Hex
 	genericRegister #(8) reelRegA(lfsr_bus, clk, reset, spinA, reelA_out);
-	genericRegister #(8) reelRegB(lfsr_bus, clk, reset, spinB, reelB_out);
-	genericRegister #(8) reelRegC(lfsr_bus, clk, reset, spinC, reelC_out);
+	genericRegister #(8) reelRegB(reelA_out, clk, reset, spinB, reelB_out);
+	genericRegister #(8) reelRegC(reelB_out, clk, reset, spinC, reelC_out);
 	
 	// Slow Machine ALU
 	smALU calculator(reelA_out, reelB_out, reelC_out, jackpot, pairWin);
