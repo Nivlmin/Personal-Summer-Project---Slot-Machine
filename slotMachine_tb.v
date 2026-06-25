ebdffe76bb5ddf934876136cbe174f37c875c90b
huynhmel <138836790+Nivlmin@users.noreply.github.com>
2026-06-25 02:36:39 -0600

    Create slotMachine_tb.v

---
+++ slotMachine_tb.v
@@ -0,0 +1,62 @@
+module slotMachine_tb();
+
+    reg clk, reset, coin_in;
+    reg [7:0] test_seed;
+
+    wire jackpot, pairWin;
+    wire [1:0] systemState;
+    wire [6:0] hexA, hexB, hexC;
+	 
+	 // Test Slot Machine
+    slotMachine uut(clk, reset, coin_in, test_seed, jackpot, pairWin, systemState, hexA, hexB, hexC);
+
+    // Clock Generator Loop
+    always begin
+        #5 clk = ~clk;
+    end
+
+    initial begin
+        // For EDA Playground to draw graphical Timing Diagram waves
+        $dumpfile("dump.vcd");
+        $dumpvars(0, slotMachine_tb);
+
+        // Initial State & Reset
+        clk = 0;
+        reset = 0;      // Put system into Reset
+        coin_in = 0;
+        test_seed = 8'h00;
+        #20;            // Wait 20 time units
+        
+        // Spin Test 1: Seed '8'hA5'
+        test_seed = 8'hA5; // Set first random starting seed
+        reset = 0;         // Keep reset low momentarily to latch it
+        #10;
+        reset = 1;         // System goes to IDLE state (00)
+        #20;
+
+        // Insert Coin for Spin 1
+        coin_in = 1;    
+        #10;            
+        coin_in = 0;    
+        
+        #1100; // Wait for Spin 1 sequence to finish completely
+
+        // Spin Test 2: Seed '8'h3C' 
+        #50;               // Let the system rest briefly in IDLE
+        test_seed = 8'h3C; // Set a completely different starting seed!
+        reset = 0;        
+        #20;
+        reset = 1;         // Release reset to let Reel A wake up holding 8'h3C
+        #20;
+
+        // Insert Coin for Spin 2
+        coin_in = 1;    
+        #10;            
+        coin_in = 0;    
+        
+        #1200; 
+
+        $finish;
+    end
+
+endmodule
